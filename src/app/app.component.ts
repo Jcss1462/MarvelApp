3821f33f219948d3695d9c65e0bb090122293816
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-11-26 19:22:40 -0500

    Se configura toaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,4 +11,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'MarvelApp';
+
+  constructor(private toastr: ToastrService){}
+
+  showToaster(){
+    this.toastr.success("Mensaje enviado con éxito");
+  }
 }
