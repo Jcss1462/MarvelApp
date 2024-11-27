7bca34f1e90b627161f278906e8ee896c0baaa5a
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-11-26 22:13:38 -0500

    Creacion inicial de componentes a utilizar

---
+++ src/app/domains/coimics/comic-fav/comic-fav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comic-fav',
+  imports: [],
+  templateUrl: './comic-fav.component.html',
+  styleUrl: './comic-fav.component.css'
+})
+export class ComicFavComponent {
+
+}
---
+++ src/app/domains/coimics/comic-list/comic-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comic-list',
+  imports: [],
+  templateUrl: './comic-list.component.html',
+  styleUrl: './comic-list.component.css'
+})
+export class ComicListComponent {
+
+}
