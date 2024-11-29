bedf9a918ef0e6b26033d35c12dc6a04093897dd
Juan Camilo Salazar Serna <jcss1462@gmail.com>
2024-11-29 15:11:49 -0500

    Se configura app para despliegue en firebase hosting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
         ts:"1",
         apiKey:"af151d7f127773a3fcb791de7d5fe4f2",
         hash:"145190727e9fbecbf3fa8fa166e6e8f0",
-        url: "http://gateway.marvel.com/v1/public"
+        url: "https://gateway.marvel.com/v1/public"
     },
     Firebase: {
         projectId: "marvelapp2",
